b6ba9bd06a12a1151451967f3693bfe51b1428dd
Zhi Charles Teoh <zc970520@hotmail.com>
2021-05-19 17:33:49 +1000

    Tute

--- navbar.js
+++ navbar.js
@@ -1,9 +1,6 @@
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+
   }
 }
